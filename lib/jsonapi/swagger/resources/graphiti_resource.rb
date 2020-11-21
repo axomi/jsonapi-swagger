f88326c1b297472b08d1f41c6ce9fd256394bb0e
Nic Barthelemy <nbarthel@axomi.com>
2020-11-21 15:18:52 -0800

    clean up

--- lib/jsonapi/swagger/resources/graphiti_resource.rb
+++ lib/jsonapi/swagger/resources/graphiti_resource.rb
@@ -4,35 +4,48 @@ module Jsonapi
     class GraphitiResource
       extend Forwardable
 
-      def_delegators :@fr, :attributes_to_serialize, :relationships_to_serialize, :sortable_fields,
-                           :creatable_fields, :updatable_fields, :filters, :mutable?, :transform_method
-
-      def initialize(fr)
-        @fr = fr
+      def initialize(gr)
+        @gr = gr
       end
 
-      alias attributes attributes_to_serialize
-      alias relationships relationships_to_serialize
-
       # TODO: Graphiti::Resource
+      def attributes
+        attributes_by_flag(:readable)
+      end
+
+      def relationships
+        {}.tap do |relations|
+          @gr.sideloads.values.map do |sl|
+            relations[sl.name] = OpenStruct.new(class_name: sl.resource_class.to_s)
+          end
+        end
+
+      end
+      
       def sortable_fields
-        []
+        attributes_by_flag(:sortable)
       end
 
       def creatable_fields
-        []
+        attributes_by_flag(:writable)
       end
 
       def updatable_fields
-        []
+        attributes_by_flag(:writable)
       end
 
       def filters
-        []
+        @gr.keys
       end
 
       def mutable?
-        false
+        @gr.abstract_class?
+      end
+
+      private
+
+      def attributes_by_flag(flag)
+        @gr.attributes.map {|k, v| k if v[:writable] }.compact
       end
     end
   end
